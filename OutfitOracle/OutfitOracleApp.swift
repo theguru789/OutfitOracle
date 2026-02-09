07344c34a7ddc9ec3b2474a8e8890701b0d49eb5
Guru Shanmukha Pranav Sanka <161491711+Gssanka@users.noreply.github.com>
2026-02-09 13:01:02 -0600

    Initial Commit

---
+++ OutfitOracle/OutfitOracleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutfitOracleApp.swift
+//  OutfitOracle
+//
+//  Created by Guru Sanka on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct OutfitOracleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OutfitOracleTests/OutfitOracleTests.swift
@@ -0,0 +1,17 @@
+//
+//  OutfitOracleTests.swift
+//  OutfitOracleTests
+//
+//  Created by Guru Sanka on 2/9/26.
+//
+
+import Testing
+@testable import OutfitOracle
+
+struct OutfitOracleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
